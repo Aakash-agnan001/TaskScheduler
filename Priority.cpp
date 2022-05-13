867132f9653dc91d43ba7f8a7e0587b22a8878a4
Divyank Shah <divyank.shah.2016@gmail.com>
2022-05-12 19:28:27 -0700

    add priority class implementation and header

---
+++ Priority.cpp
@@ -0,0 +1,9 @@
+#include "Priority.h"
+
+unsigned int Priority::getPriority() {
+    return this->priority;
+}
+
+void Priority::setPriority(unsigned int priority) {
+    this->priority = priority;
+}
---
+++ Priority.h
@@ -0,0 +1,12 @@
+#ifndef PRIORITY_H
+#define PRIORITY_H
+
+class Priority {
+    private:
+        unsigned int priority;
+    public:
+        void getPriority();
+        void setPriority();
+}
+
+#endif
