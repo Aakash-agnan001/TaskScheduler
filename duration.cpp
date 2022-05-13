441e56dc7c109ec83a0e3446b6e4b9299b788364
MarcusAHickman <mhick020@ucr.edu>
2022-05-13 12:22:22 -0700

    Declared class and added implementation.

---
+++ duration.cpp
@@ -0,0 +1,19 @@
+#include "duration.h"
+
+Duration::Duration(unsigned int hours, unsigned int minutes)
+{
+	this->hours = hours;
+	this->minutes = minutes;
+}
+
+string Duration::getHours()
+{
+	string hours_string = to_string(hours);
+	return hours_string;
+}
+
+string Duration::getMinutes() 
+{	string minutes_string = to_string(minutes);
+	return minutes_string;
+
+}
