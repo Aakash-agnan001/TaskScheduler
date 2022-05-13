6edfca2eef2e6bc8c7a36ab0aee78a4f51e996ef
Divyank Shah <divyank.shah.2016@gmail.com>
2022-05-12 19:33:19 -0700

    add title header

---
+++ Title.h
@@ -0,0 +1,14 @@
+#ifndef TITLE_H
+#define TITLE_H
+
+#include <string>
+
+class Title {
+    private:
+        std::string title;
+    public:
+        void getTitle();
+        void setTitle(std::string);
+};
+
+#endif
