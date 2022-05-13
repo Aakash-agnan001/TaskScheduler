e26f8aa073a2d5fe73c627d5c59a527495e9961a
MarcusAHickman <mhick020@ucr.edu>
2022-05-13 11:13:35 -0700

    Declared due_date class.

---
+++ due_date.h
@@ -0,0 +1,22 @@
+#ifndef DUE_DATE_H
+#define DUE_DATE_H
+
+#include <iostream>
+#include <string> 
+
+using namespace std;
+
+class DueDate {
+	private:
+		unsigned int day;
+		unsigned int month;
+		unsigned int year;
+	public:
+		DueDate(int day, int month, int year);
+		void setDay(int day);
+		void setMonth(int month);
+		void setYear(int year);
+		unsgned int getDate();
+};
+
+#endif
