5965dc6c904b66b0c4520f56f7bf172d3889924a
MarcusAHickman <103014279+MarcusAHickman@users.noreply.github.com>
2022-05-17 19:34:32 -0700

    Update Priority.h

--- Priority.h
+++ Priority.h
@@ -1,16 +1,14 @@
 #ifndef PRIORITY_H
 #define PRIORITY_H
 
-#include "Information.hpp"
-
-class Priority : public Information {
+class Priority {
     private:
         unsigned int priority;
     public:
 	Priority();
         Priority(unsigned int);
-        virtual void getPriority();
-        virtual void setPriority(unsigned int);
+        void getPriority();
+        void setPriority(unsigned int);
 };
 
 #endif
