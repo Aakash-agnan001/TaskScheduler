f00efce4872acf39b5872683a7978041305fa2d2
Aakash-agnan001 <aakashappiah@gmail.com>
2022-05-17 19:35:22 -0700

    refactored description test

--- Description_Tests.h
+++ Description_Tests.h
@@ -6,7 +6,25 @@
 
 TEST(DESCRIPTION, EMPTY) {
 	Description name("");
-	EXPECT_EQ(name.getDescription(), "");
+	EXPECT_EQ(name.getInfo(), "");
+}
+
+TEST(DESCRIPTION, ONE_WORD) {
+	Description name("");
+	name.setInfo("Hello");
+	EXPECT_EQ(name.getInfo(), "Hello");
+}
+
+TEST(DESCRIPTION, MANY_WORDS) {
+	Description name("");
+	name.setInfo("Hello Aakash, hi");
+	EXPECT_EQ(name.getInfo(), "Hello Aakash, hi");
+}
+
+TEST(DESCRIPTION, SPECIAL_CHARACTERS) {
+	Description name("");
+	name.setInfo("@@k@$h! ! !");
+	EXPECT_EQ(name.getInfo(), "@@k@$h! ! !");
 }
 
 #endif
