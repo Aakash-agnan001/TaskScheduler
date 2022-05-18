8614a25b3eda334c519c6716c299495a7f7915aa
MarcusAHickman <103014279+MarcusAHickman@users.noreply.github.com>
2022-05-17 17:24:03 -0700

    Create Classification_Tests.hpp
    Added Classification tests.

---
+++ Classification_Tests.hpp
@@ -0,0 +1,23 @@
+#ifndef __CLASSIFICATION_TESTS_H__
+#define __CLASSIFICATION_TESTS_H__
+
+#include "gtest/gtest.h"
+#include "Classification.h"
+
+TEST(CLASS, DefConstruct) {
+    Classification test_class = new Classification(); 
+    EXPECT_EQ(test_class.GetClassification(), 0);
+}
+
+TEST(CLASS, PassRegParam) {
+    Classification test_class = new Classification(4);
+    EXPECT_EQ(test_class.GetClassification(), 4);
+}
+
+TEST(Class, TestSetClass) {
+    Classification test_class = new Classification();
+    test_class.SetClassification(4);
+    EXPECT_EQ(test_class.GetClassification(), 4);
+}
+
+#endif
