4cbed952e12c94a856decab09757dac0e8fe62a0
Divyank Shah <divyank.shah.2016@gmail.com>
2022-05-18 02:38:33 -0700

    make duration not abstract

--- duration.h
+++ duration.h
@@ -12,6 +12,9 @@ public:
     Duration(unsigned minutes);
     virtual void setInfo(unsigned int);
     virtual unsigned int getInfo(unsigned int);
+
+    virtual void setInfo(std::string) {};
+    virtual std::string getInfo(std::string) {return "";};
 };
 
 #endif
