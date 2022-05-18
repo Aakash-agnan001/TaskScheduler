e899e7bb9be893d16a3119a3997f35e00c780323
MarcusAHickman <103014279+MarcusAHickman@users.noreply.github.com>
2022-05-17 17:49:54 -0700

    Update Classification_Tests.h
    Fixed Class tests

--- Classification_Tests.h
+++ Classification_Tests.h
@@ -5,17 +5,17 @@
 #include "Classification.h"
 
 TEST(CLASS, DefConstruct) {
-    Classification test_class = new Classification(); 
+    Classification test_class = Classification(); 
     EXPECT_EQ(test_class.GetClassification(), 0);
 }
 
 TEST(CLASS, PassRegParam) {
-    Classification test_class = new Classification(4);
+    Classification test_class = Classification(4);
     EXPECT_EQ(test_class.GetClassification(), 4);
 }
 
 TEST(Class, TestSetClass) {
-    Classification test_class = new Classification();
+    Classification test_class = Classification();
     test_class.SetClassification(4);
     EXPECT_EQ(test_class.GetClassification(), 4);
 }
