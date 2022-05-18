6c6aaaac422ad965726c9dd0336e888e4beb2d49
Aakash Gnanakumar <aakgna@ucr-secure-26-10-13-153-222.wnet.ucr.edu>
2022-05-17 20:00:16 -0700

    needs me to commit to pull changes

--- cmake_install.cmake
+++ cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: /Users/shahdivyank/Documents/final-project-error-404
+# Install script for directory: /Users/aakgna/Documents/CS100/final-project-error-404
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -39,7 +39,7 @@ endif()
 
 if(NOT CMAKE_INSTALL_LOCAL_ONLY)
   # Include the install script for each subdirectory.
-  include("/Users/shahdivyank/Documents/final-project-error-404/googletest/cmake_install.cmake")
+  include("/Users/aakgna/Documents/CS100/final-project-error-404/googletest/cmake_install.cmake")
 
 endif()
 
@@ -51,5 +51,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "/Users/shahdivyank/Documents/final-project-error-404/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "/Users/aakgna/Documents/CS100/final-project-error-404/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
