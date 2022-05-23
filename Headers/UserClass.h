ee58183f00d680c09d236afdb2b8351a6ba90588
azhao036 <azhao036@ucr.edu>
2022-05-22 17:17:29 -0700

    added UserClass.h

---
+++ Headers/UserClass.h
@@ -0,0 +1,14 @@
+#ifndef USERCLASS_H
+#define USERCLASS_H
+
+#include "TaskList.h"
+
+class UserClass {
+	private:
+	string firstName;
+	string lastName;
+	string password;
+	TaskList tasks;
+}
+
+#endif
